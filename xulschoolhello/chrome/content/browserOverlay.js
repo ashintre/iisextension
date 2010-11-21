acda3e4a5876b36faa5f54ad120ff51300095b51
shrivastavkhyati@gmail.com <shrivastavkhyati@gmail.com@a80b9f50-1de1-997a-6561-d283bcb1a517>
2010-11-21 17:07:55 +0000

    Description: Checking in folder structure for extension development.

---
+++ xulschoolhello/chrome/content/browserOverlay.js
@@ -0,0 +1,15 @@
+if("undefined" == typeof(XULSchoolChrome)){
+var XULSchoolChrome = {};
+};
+
+XULSchoolChrome.BrowserOverlay = {
+
+sayHello : function(aEvent)
+{
+var random_number = Math.floor(Math.random()*10000000 +1);
+var locate = window.location.toString();
+window.alert("URL: " + window.location.href);
+
+}
+
+}
